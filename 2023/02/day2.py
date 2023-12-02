bf306fd9c8995a4f2a9085ca96080d5173f79384
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2023-12-02 00:01:05 -0800

    Day 2 part 2

--- 2023/02/day2.py
+++ 2023/02/day2.py
@@ -1,3 +1,5 @@
+from math import prod
+
 with open("input", "rt") as fh:
     game_input = [line.strip() for line in fh.readlines()]
 
@@ -8,11 +10,13 @@ bag_cubes = {
 }
 
 possible_games = 0
+min_cubes_power_sum = 0
 for game in game_input:
     impossible = False
     game_id, game_info = game.split(":")
     _, game_num = game_id.split(" ")
     game_sets = game_info.split(";")
+    minimum_cubes_needed = {}
     for game_set in game_sets:
         game_cubes_total = {}
         set_cubes = game_set.split(",")
@@ -25,18 +29,28 @@ for game in game_input:
         for cube_total in game_cubes_total.items():
             cube_total_color, cube_total_count = cube_total
             try:
+                if (
+                    game_cubes_total[cube_total_color]
+                    > minimum_cubes_needed[cube_total_color]
+                ):
+                    minimum_cubes_needed[cube_total_color] = cube_total_count
+            except KeyError:
+                minimum_cubes_needed[cube_total_color] = cube_total_count
+            try:
                 if game_cubes_total[cube_total_color] > bag_cubes[cube_total_color]:
                     impossible = True
                     print(f"game {game_num} - {cube_total}")
-                    break
             except KeyError:
                 pass
-        if impossible == True:
-            break
+    minimum_cubes_power = prod(minimum_cubes_needed.values())
+    min_cubes_power_sum += minimum_cubes_power
+    # print(f"min_needed: {minimum_cubes_needed.items()}")
+    print(f"min_power: {minimum_cubes_power}")
     if impossible == False:
         possible_games += int(game_num)
-        print(f"POSSIBLE: {game_num}")
+        # print(f"POSSIBLE: {game_num}")
 
 # sum_of_ids = sum(map(int, possible_games))
-print(possible_games)
+print(f"part 1: {possible_games}")
+print(f"part 2: {min_cubes_power_sum}")
 # print(f"part 1: {sum_of_ids}")
